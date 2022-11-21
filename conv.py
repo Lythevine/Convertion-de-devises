b54b0f9896e7114b9dc119a0c928c408d6d7f209
lythevine <lythevinedomche@gmail.com>
2022-11-21 11:25:33 +0100

    Ajout d'une devise supplémentaire

--- conv.py
+++ conv.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 reponce = "oui"
 while reponce == "oui":
-	monnaie=input("De quelle monnaie disposez vous ? Dollars, FCFA, Euro   ")
+	monnaie=input("De quelle monnaie disposez vous ? Dollars, FCFA, Euro, yuan ")
 	monnaie=str(monnaie)
 
 	devise=input("En quelle devise voulez vous là changer ?   ")
@@ -12,10 +12,13 @@ while reponce == "oui":
 	print("1 Dollars = 550FCFA")
 	print("1 Euro = 600 FCFA")
 	print("1 Dollars = 2 Euro")
+	print("1 yuan = 88 FCFA")
+	print("1 yuan = 0.14 Dollars ")
 	print(" ")
 
 	montant=input("Entrez le montant que vous voulez changer:    ")
 	montant=int(montant)
+	
 
 	if monnaie=="Dollars" and devise=="FCFA":
 		resultat==montant*550
@@ -41,10 +44,34 @@ while reponce == "oui":
 		resultat=montant/2
 		resultat=float(resultat)
 
+	elif monnaie=="yuan" and devise=="FCFA":
+		resultat=montant*88
+		resultat=float(resultat)
+
+	elif monnaie=="FCFA" and devise=="yuan":
+		resultat=montant/88
+		resultat=float(resultat)
+
+	elif monnaie=="yuan" and devise=="Dollars":
+		resultat=montant*0.14
+		resultat=float(resultat)
+
+	elif monnaie=="Dollars" and devise=="yuan":
+		resultat=montant/0.14
+		resultat=float(resultat)
+
+	elif monnaie=="yuan" and devise=="Euro":
+		resultat=montant*0.14
+		resultat=float(resultat)
+
+	elif monnaie=="Euro" and devise=="yuan":
+		resultat=montant/0.14
+		resultat=float(resultat)		
+
 	else:
 		print("Nous ne disposons pas de cette monnaie   ")
 
-	decision=input(f" { resultat } " f" { devise }est la somme que vous obtiendrez après convertion. Entrez oui pour valider l'operation et non pour l'annulée   ")
+	decision=input(f" { resultat } " f" { devise } est la somme que vous obtiendrez après convertion. Entrez oui pour valider l'operation et non pour l'annulée   ")
 	decision =str(decision)	
 
 	if decision == "oui":
@@ -57,6 +84,12 @@ while reponce == "oui":
 
 	reponce = print("Voulez vous effectuez une autre convertion ?   ")
 	reponce =str(reponce)
+
+		
+		
+
+
+
 
 		
 		
