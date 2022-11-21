d73c6046d3a1e4a3231f556f7eb7d0af93005951
lythevine <lythevinedomche@gmail.com>
2022-11-21 10:21:21 +0100

    initialisation

---
+++ conv.py
@@ -0,0 +1,64 @@
+#coding:utf-8
+reponce = "oui"
+while reponce == "oui":
+	monnaie=input("De quelle monnaie disposez vous ? Dollars, FCFA, Euro   ")
+	monnaie=str(monnaie)
+
+	devise=input("En quelle devise voulez vous là changer ?   ")
+	devise=str(devise)
+	print(" ")
+	print("***********PRESENTATION DES TAUX DE CHANGE*************")
+	print(" ")
+	print("1 Dollars = 550FCFA")
+	print("1 Euro = 600 FCFA")
+	print("1 Dollars = 2 Euro")
+	print(" ")
+
+	montant=input("Entrez le montant que vous voulez changer:    ")
+	montant=int(montant)
+
+	if monnaie=="Dollars" and devise=="FCFA":
+		resultat==montant*550
+		resultat=float(resultat)
+
+	elif monnaie=="FCFA" and devise=="Dollars":
+		resultat=montant/550
+		resultat=float(resultat)
+
+	elif monnaie=="Euro" and devise=="FCFA":
+		resultat=montant*600
+		resultat=float(resultat)
+
+	elif monnaie=="FCFA" and devise=="Euro":
+		resultat=montant/600
+		resultat=float(resultat)
+
+	elif monnaie=="Dollars" and devise=="Euro":
+		resultat=montant*2
+		resultat=float(resultat)
+
+	elif monnaie=="Euro" and devise=="Dollars":
+		resultat=montant/2
+		resultat=float(resultat)
+
+	else:
+		print("Nous ne disposons pas de cette monnaie   ")
+
+	decision=input(f" { resultat } " f" { devise }est la somme que vous obtiendrez après convertion. Entrez oui pour valider l'operation et non pour l'annulée   ")
+	decision =str(decision)	
+
+	if decision == "oui":
+		print("Opération effectuée avec succès   ")
+	elif decision == "non":
+		print("Opération annulée   ")
+
+	else:
+		print("....")
+
+	reponce = print("Voulez vous effectuez une autre convertion ?   ")
+	reponce =str(reponce)
+
+		
+		
+
+
